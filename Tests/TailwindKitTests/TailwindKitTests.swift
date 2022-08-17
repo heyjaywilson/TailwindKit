2694fc856581a07ee2dc7cebb1fbe5ca75c19478
mwilson <maegan@maeganwilson.com>
2022-08-16 21:33:32 -0500

    test: test for flexbox

--- Tests/TailwindKitTests/TailwindKitTests.swift
+++ Tests/TailwindKitTests/TailwindKitTests.swift
@@ -2,10 +2,17 @@ import XCTest
 @testable import TailwindKit
 
 final class TailwindKitTests: XCTestCase {
-    func testExample() throws {
+    func testFlexboxFlex() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(TailwindKit().text, "Hello, World!")
+        XCTAssertEqual(Tailwind.flex([.flex]), "flex")
+    }
+
+    func testFlexboxMultipleProps() throws {
+        let sut = Tailwind.self
+        let expectedOutput = "flex flex-row-reverse"
+
+        XCTAssertEqual(sut.flex([.flex, .rowReverse]), expectedOutput)
     }
 }
